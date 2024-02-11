3db68bfed7f551b68788db5fbf9454a5f9cddc0e
Ethan Wiegert <ewiegert99@gmail.com>
2024-02-11 14:57:57 -0500

    Initial changes to project.

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require("gray-matter");
 
 async function generate() {
   const feed = new RSS({
-    title: "Your Name",
-    site_url: "https://yoursite.com",
-    feed_url: "https://yoursite.com/feed.xml",
+    title: "Ethan Wiegert",
+    site_url: "https://ethanwiegert.com",
+    feed_url: "https://ethanwiegert.com/feed.xml",
   });
 
   const posts = await fs.readdir(path.join(__dirname, "..", "pages", "posts"));
